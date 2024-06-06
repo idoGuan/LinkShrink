2d0330ec26f7063af0205dc5ded2f34e92874f3a
idoGuan <1760785369@qq.com>
2024-06-06 14:59:54 +0800

    提交MySQL数据库分页插件

---
+++ project/src/main/java/com/xiaoguan/shortlink/project/config/DataBaseConfiguration.java
@@ -0,0 +1,30 @@
+package com.xiaoguan.shortlink.project.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * ClassName: DataBaseConfiguration
+ * Package: com.xiaoguan.shortlink.project.common.config
+ * Description:
+ *
+ * @Author 小管不要跑
+ * @Create 2024/6/6 14:58
+ * @Version 1.0
+ */
+@Configuration
+public class DataBaseConfiguration {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
