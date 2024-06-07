80bc0eb3a865a605fa2c19f5ff9ea8bc7f2898ea
idoGuan <1760785369@qq.com>
2024-06-07 14:23:04 +0800

    短链接跳转原始链接开发缓存击穿功能

---
+++ project/src/main/java/com/xiaoguan/shortlink/project/common/constant/RedisKeyConstant.java
@@ -0,0 +1,23 @@
+package com.xiaoguan.shortlink.project.common.constant;
+
+/**
+ * ClassName: RedisKeyConstant
+ * Package: com.xiaoguan.shortlink.project.common.constant
+ * Description:
+ *
+ * @Author 小管不要跑
+ * @Create 2024/6/7 14:15
+ * @Version 1.0
+ */
+public class RedisKeyConstant {
+
+    /**
+     * 短链接跳转前缀 Key
+     */
+    public static final String GOTO_SHORT_LINK_KEY = "short-link_goto_%s";
+
+    /**
+     * 短链接跳转锁前缀 Key
+     */
+    public static final String LOCK_GOTO_SHORT_LINK_KEY = "short-link_lock_goto_%s";
+}
