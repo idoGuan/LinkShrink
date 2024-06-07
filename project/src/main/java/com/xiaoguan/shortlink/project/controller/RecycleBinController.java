a5c9bfbd88ff5b0aab3fddbd1cfbb9527a6278ed
idoGuan <1760785369@qq.com>
2024-06-07 17:00:47 +0800

    开发短链接分页查询回收站功能

--- project/src/main/java/com/xiaoguan/shortlink/project/controller/RecycleBinController.java
+++ project/src/main/java/com/xiaoguan/shortlink/project/controller/RecycleBinController.java
@@ -1,10 +1,14 @@
 package com.xiaoguan.shortlink.project.controller;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.xiaoguan.shortlink.project.common.convention.result.Result;
 import com.xiaoguan.shortlink.project.common.convention.result.Results;
 import com.xiaoguan.shortlink.project.dto.req.RecycleBinSaveReqDTO;
+import com.xiaoguan.shortlink.project.dto.req.ShortLinkPageReqDTO;
+import com.xiaoguan.shortlink.project.dto.resp.ShortLinkPageRespDTO;
 import com.xiaoguan.shortlink.project.service.RecycleBinService;
 import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,5 +35,13 @@ public class RecycleBinController {
     public Result<Void> saveRecycleBin(@RequestBody RecycleBinSaveReqDTO requestParam) {
         recycleBinService.saveRecycleBin(requestParam);
         return Results.success();
+    }
+
+    /**
+     * 分页查询回收站短链接
+     */
+    @GetMapping("/api/short-link/v1/recycle-bin/page")
+    public Result<IPage<ShortLinkPageRespDTO>> pageShortLink(ShortLinkPageReqDTO requestParam) {
+        return Results.success(recycleBinService.pageShortLink(requestParam));
     }
 }
