cb7873ffbf2e442c9e618b2fded5f2dafeec5ca7
idoGuan <1760785369@qq.com>
2024-06-08 17:43:11 +0800

    开发短链接监控之分组短链接访问记录

--- admin/src/main/java/com/xiaoguan/shortlink/admin/controller/ShortLinkStatsController.java
+++ admin/src/main/java/com/xiaoguan/shortlink/admin/controller/ShortLinkStatsController.java
@@ -3,6 +3,7 @@ package com.xiaoguan.shortlink.admin.controller;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.xiaoguan.shortlink.admin.common.convention.result.Result;
 import com.xiaoguan.shortlink.admin.remote.ShortLinkRemoteService;
+import com.xiaoguan.shortlink.admin.remote.dto.req.ShortLinkGroupStatsAccessRecordReqDTO;
 import com.xiaoguan.shortlink.admin.remote.dto.req.ShortLinkGroupStatsReqDTO;
 import com.xiaoguan.shortlink.admin.remote.dto.req.ShortLinkStatsAccessRecordReqDTO;
 import com.xiaoguan.shortlink.admin.remote.dto.req.ShortLinkStatsReqDTO;
@@ -47,5 +48,13 @@ public class ShortLinkStatsController {
     @GetMapping("/api/short-link/admin/v1/stats/group")
     public Result<ShortLinkStatsRespDTO> groupShortLinkStats(ShortLinkGroupStatsReqDTO requestParam) {
         return shortLinkRemoteService.groupShortLinkStats(requestParam);
+    }
+
+    /**
+     * 访问分组短链接指定时间内访问记录监控数据
+     */
+    @GetMapping("/api/short-link/admin/v1/stats/access-record/group")
+    public Result<IPage<ShortLinkStatsAccessRecordRespDTO>> groupShortLinkStatsAccessRecord(ShortLinkGroupStatsAccessRecordReqDTO requestParam) {
+        return shortLinkRemoteService.groupShortLinkStatsAccessRecord(requestParam);
     }
 }
