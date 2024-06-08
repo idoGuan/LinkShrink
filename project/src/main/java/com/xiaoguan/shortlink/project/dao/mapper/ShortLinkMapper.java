8baaca3c12fb9a0b4aaa12c70442b4d7e6e8a01f
idoGuan <1760785369@qq.com>
2024-06-08 16:34:37 +0800

    开发短链接监控之汇总统计

--- project/src/main/java/com/xiaoguan/shortlink/project/dao/mapper/ShortLinkMapper.java
+++ project/src/main/java/com/xiaoguan/shortlink/project/dao/mapper/ShortLinkMapper.java
@@ -2,6 +2,8 @@ package com.xiaoguan.shortlink.project.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.xiaoguan.shortlink.project.dao.entity.ShortLinkDO;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Update;
 
 /**
  * ClassName: LinkMapper
@@ -13,4 +15,16 @@ import com.xiaoguan.shortlink.project.dao.entity.ShortLinkDO;
  * @Version 1.0
  */
 public interface ShortLinkMapper extends BaseMapper<ShortLinkDO> {
+
+    /**
+     * 短链接访问统计自增
+     */
+    @Update("update t_link set total_pv = total_pv + #{totalPv}, total_uv = total_uv + #{totalUv}, total_uip = total_uip + #{totalUip} where gid = #{gid} and full_short_url = #{fullShortUrl}")
+    void incrementStats(
+            @Param("gid") String gid,
+            @Param("fullShortUrl") String fullShortUrl,
+            @Param("totalPv") Integer totalPv,
+            @Param("totalUv") Integer totalUv,
+            @Param("totalUip") Integer totalUip
+    );
 }
